2edcf17781c9469694633622fa480cee1b7fd3f2
Koussay Akchi <koussay.akchi@wecraft.tn>
2024-10-20 00:07:16 +0100

    fix: removed planet

--- components/features-planet.tsx
+++ components/features-planet.tsx
@@ -8,72 +8,19 @@ import PlanetTagImg04 from "@/public/images/planet-tag-04.png";
 
 export default function FeaturesPlanet() {
   return (
-    <section className="relative before:absolute before:inset-0 before:-z-20 before:bg-gray-900">
+    <section className="relative before:absolute before:inset-0 before:-z-20">
       <div className="mx-auto max-w-6xl px-4 sm:px-6">
         <div className="py-12 md:py-20">
           {/* Section header */}
           <div className="mx-auto max-w-3xl pb-16 text-center md:pb-20">
-            <h2 className="text-3xl font-bold text-gray-200 md:text-4xl">
-              Simple helps your teams work more efficiently together
+            <h2 className="text-3xl font-bold md:text-4xl">
+              Reach a global market with our network
             </h2>
-          </div>
-          {/* Planet */}
-          <div className="pb-16 md:pb-20" data-aos="zoom-y-out">
-            <div className="text-center">
-              <div className="relative inline-flex rounded-full before:absolute before:inset-0 before:-z-10 before:scale-[.85] before:animate-[pulse_4s_cubic-bezier(.4,0,.6,1)_infinite] before:bg-gradient-to-b before:from-blue-900 before:to-sky-700/50 before:blur-3xl after:absolute after:inset-0 after:rounded-[inherit] after:[background:radial-gradient(closest-side,theme(colors.blue.500),transparent)]">
-                <Image
-                  className="rounded-full bg-gray-900"
-                  src={PlanetImg}
-                  width={400}
-                  height={400}
-                  alt="Planet"
-                />
-                <div className="pointer-events-none" aria-hidden="true">
-                  <Image
-                    className="absolute -right-64 -top-20 z-10 max-w-none"
-                    src={PlanetOverlayImg}
-                    width={789}
-                    height={755}
-                    alt="Planet decoration"
-                  />
-                  <div>
-                    <Image
-                      className="absolute -left-28 top-16 z-10 animate-[float_4s_ease-in-out_infinite_both] opacity-80 transition-opacity duration-500"
-                      src={PlanetTagImg01}
-                      width={253}
-                      height={56}
-                      alt="Tag 01"
-                    />
-                    <Image
-                      className="absolute left-56 top-7 z-10 animate-[float_4s_ease-in-out_infinite_1s_both] opacity-30 transition-opacity duration-500"
-                      src={PlanetTagImg02}
-                      width={241}
-                      height={56}
-                      alt="Tag 02"
-                    />
-                    <Image
-                      className="absolute -left-20 bottom-24 z-10 animate-[float_4s_ease-in-out_infinite_2s_both] opacity-25 transition-opacity duration-500"
-                      src={PlanetTagImg03}
-                      width={243}
-                      height={56}
-                      alt="Tag 03"
-                    />
-                    <Image
-                      className="absolute bottom-32 left-64 z-10 animate-[float_4s_ease-in-out_infinite_3s_both] opacity-80 transition-opacity duration-500"
-                      src={PlanetTagImg04}
-                      width={251}
-                      height={56}
-                      alt="Tag 04"
-                    />
-                  </div>
-                </div>
-              </div>
-            </div>
           </div>
           {/* Grid */}
           <div className="grid overflow-hidden sm:grid-cols-2 lg:grid-cols-3 [&>*]:relative [&>*]:p-6 [&>*]:before:absolute [&>*]:before:bg-gray-800 [&>*]:before:[block-size:100vh] [&>*]:before:[inline-size:1px] [&>*]:before:[inset-block-start:0] [&>*]:before:[inset-inline-start:-1px] [&>*]:after:absolute [&>*]:after:bg-gray-800 [&>*]:after:[block-size:1px] [&>*]:after:[inline-size:100vw] [&>*]:after:[inset-block-start:-1px] [&>*]:after:[inset-inline-start:0] md:[&>*]:p-10">
             <article>
-              <h3 className="mb-2 flex items-center space-x-2 font-medium text-gray-200">
+              <h3 className="mb-2 flex items-center space-x-2 font-medium ">
                 <svg
                   className="fill-blue-500"
                   xmlns="http://www.w3.org/2000/svg"
@@ -84,14 +31,14 @@ export default function FeaturesPlanet() {
                 </svg>
                 <span>Instant Analytics</span>
               </h3>
-              <p className="text-[15px] text-gray-400">
+              <p className="text-[15px] ">
                 Collect essential insights about how visitors are using your
                 site with in-depth page view metrics like pages, referring
                 sites, and more.
               </p>
             </article>
             <article>
-              <h3 className="mb-2 flex items-center space-x-2 font-medium text-gray-200">
+              <h3 className="mb-2 flex items-center space-x-2 font-medium ">
                 <svg
                   className="fill-blue-500"
                   xmlns="http://www.w3.org/2000/svg"
@@ -102,14 +49,14 @@ export default function FeaturesPlanet() {
                 </svg>
                 <span>Metadata</span>
               </h3>
-              <p className="text-[15px] text-gray-400">
+              <p className="text-[15px] ">
                 Collect essential insights about how visitors are using your
                 site with in-depth page view metrics like pages, referring
                 sites, and more.
               </p>
             </article>
             <article>
-              <h3 className="mb-2 flex items-center space-x-2 font-medium text-gray-200">
+              <h3 className="mb-2 flex items-center space-x-2 font-medium ">
                 <svg
                   className="fill-blue-500"
                   xmlns="http://www.w3.org/2000/svg"
@@ -124,14 +71,14 @@ export default function FeaturesPlanet() {
                 </svg>
                 <span>SEO &amp; Performance</span>
               </h3>
-              <p className="text-[15px] text-gray-400">
+              <p className="text-[15px] ">
                 Collect essential insights about how visitors are using your
                 site with in-depth page view metrics like pages, referring
                 sites, and more.
               </p>
             </article>
             <article>
-              <h3 className="mb-2 flex items-center space-x-2 font-medium text-gray-200">
+              <h3 className="mb-2 flex items-center space-x-2 font-medium ">
                 <svg
                   className="fill-blue-500"
                   xmlns="http://www.w3.org/2000/svg"
@@ -142,14 +89,14 @@ export default function FeaturesPlanet() {
                 </svg>
                 <span>Custom Code</span>
               </h3>
-              <p className="text-[15px] text-gray-400">
+              <p className="text-[15px] ">
                 Collect essential insights about how visitors are using your
                 site with in-depth page view metrics like pages, referring
                 sites, and more.
               </p>
             </article>
             <article>
-              <h3 className="mb-2 flex items-center space-x-2 font-medium text-gray-200">
+              <h3 className="mb-2 flex items-center space-x-2 font-medium ">
                 <svg
                   className="fill-blue-500"
                   xmlns="http://www.w3.org/2000/svg"
@@ -160,14 +107,14 @@ export default function FeaturesPlanet() {
                 </svg>
                 <span>Localization</span>
               </h3>
-              <p className="text-[15px] text-gray-400">
+              <p className="text-[15px] ">
                 Collect essential insights about how visitors are using your
                 site with in-depth page view metrics like pages, referring
                 sites, and more.
               </p>
             </article>
             <article>
-              <h3 className="mb-2 flex items-center space-x-2 font-medium text-gray-200">
+              <h3 className="mb-2 flex items-center space-x-2 font-medium ">
                 <svg
                   className="fill-blue-500"
                   xmlns="http://www.w3.org/2000/svg"
@@ -178,7 +125,7 @@ export default function FeaturesPlanet() {
                 </svg>
                 <span>Canonical URL</span>
               </h3>
-              <p className="text-[15px] text-gray-400">
+              <p className="text-[15px]">
                 Collect essential insights about how visitors are using your
                 site with in-depth page view metrics like pages, referring
                 sites, and more.
